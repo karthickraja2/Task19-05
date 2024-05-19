99ad79bffa16cf8cdcac5478fa2c2b4f8ab8b4c0
Karthickraja <103241167+karthickraja2@users.noreply.github.com>
2024-05-19 19:01:49 +0530

    Add files via upload
    Creating a form using html and return the data to the console

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo() {
+    var firstname = document.getElementById("firstname").value;
+    var middlename = document.getElementById("middlename").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    console.log(firstname);
+    console.log(middlename);
+    console.log(lastname);
+    console.log(email);
+    console.log(password);
+}
